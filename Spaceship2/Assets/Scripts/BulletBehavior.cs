febd2c14447ed538285330fd8e47364c5befc3c7
murkri229 <mikel.urquijo@opendeusto.es>
2026-05-06 13:14:09 +0200

    Scripts
    The same scripts from the old project have been implemented

---
+++ Spaceship2/Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+
+    public float lifeTime;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeTime -= Time.deltaTime;
+        if (lifeTime < 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
