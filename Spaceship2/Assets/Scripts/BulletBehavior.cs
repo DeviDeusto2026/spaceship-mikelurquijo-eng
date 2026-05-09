e6fc549e3280863ab760748cf789d8df9a1ae1c0
murkri229 <mikel.urquijo@opendeusto.es>
2026-05-09 11:18:41 +0200

    Bullet Behavior & Spawn
    Spawn works correctly, but bullet behaviour is not finished

--- Spaceship2/Assets/Scripts/BulletBehavior.cs
+++ Spaceship2/Assets/Scripts/BulletBehavior.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 public class BulletBehavior : MonoBehaviour
 {
 
-    public float lifeTime;
+    public GameObject bullet;
+    public float lifeTime = 2.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,19 +14,11 @@ public class BulletBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        lifeTime -= Time.deltaTime;
-        if (lifeTime < 0)
-        {
-            Destroy(this.gameObject);
-        }
+        Destroy(bullet, lifeTime);
     }
 
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
-    //    {
-    //        Destroy(collision.gameObject);
-    //        Destroy(this.gameObject);
-    //    }
-    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(bullet);
+    }
 }
--- Spaceship2/Assets/Scripts/Spawn.cs
+++ Spaceship2/Assets/Scripts/Spawn.cs
@@ -1,11 +1,23 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
+
+    public GameObject enemy;
+
+    //public int rows = 2;
+    public int distance = 20;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+        Vector3 position = new Vector3(enemy.transform.position.x + distance, enemy.transform.position.y, enemy.transform.position.z); 
+        Vector3 position2 = new Vector3(enemy.transform.position.x - distance, enemy.transform.position.y, enemy.transform.position.z);
+        Instantiate(enemy, position, Quaternion.identity); 
+        Instantiate(enemy, position2, Quaternion.identity);
+
     }
 
     // Update is called once per frame
